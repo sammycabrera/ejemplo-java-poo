4a0054420894fcd912e1c5455068be661ffae9b7
Sammy Cabrera <systematicsammy@gmail.com>
2016-03-02 18:50:35 -0500

    publicar proyecto

---
+++ EjercicioPOO/src/edu/sena/EjercicioPOO.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.sena;
+
+/**
+ *
+ * @author Sammy Cabrera
+ */
+public class EjercicioPOO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola mundo java");
+    }
+    
+}
